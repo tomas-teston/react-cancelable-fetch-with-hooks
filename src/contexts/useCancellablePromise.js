949ca8ba088410c14d96f05f1efc808a788f392f
Tomás Muñoz Testón <tomini18@hotmail.com>
2019-05-30 16:00:45 +0200

    creation of fetch with possibility of cancellation

--- src/contexts/useCancellablePromise.js
+++ src/contexts/useCancellablePromise.js
@@ -1,51 +1,88 @@
-import { useRef, useEffect } from "react";
+// export function makeCancelable(promise) {
+//   let isCanceled = false;
 
-export function makeCancelable(promise) {
-  let isCanceled = false;
+//   const wrappedPromise = new Promise((resolve, reject) => {
+//     promise
+//       .then(val => (isCanceled ? console.log("cancelado") : resolve(val)))
+//       .catch(error =>
+//         isCanceled ? reject(new Error({ isCanceled })) : reject(error)
+//       );
+//   });
 
-  const wrappedPromise = new Promise((resolve, reject) => {
-    promise
-      .then(val =>
-        isCanceled ? reject(new Error({ isCanceled })) : resolve(val)
-      )
-      .catch(error =>
-        isCanceled ? reject(new Error({ isCanceled })) : reject(error)
-      );
-  });
+//   return {
+//     promise: wrappedPromise,
+//     cancel() {
+//       isCanceled = true;
+//     }
+//   };
+// }
 
-  return {
-    promise: wrappedPromise,
-    cancel() {
-      isCanceled = true;
-    }
+// const createPromise = () => {
+//   return fetch("https://tomini182.free.beeceptor.com/prueba", {
+//     mode: "cors",
+//     headers: {
+//       "Access-Control-Allow-Origin": "*"
+//     }
+//   });
+// };
+
+export default function useCancellablePromise() {
+  let controller;
+
+  const init = () => {
+    controller = new window.AbortController();
   };
-}
 
-export default function useCancellablePromise(cancelable = makeCancelable) {
-  const emptyPromise = Promise.resolve(true);
+  init();
+
+  const handleTriggerFetch = () => {
+    debugger;
+    fetch("https://tomini18.getsandbox.com/delay", {
+      signal: controller.signal,
+      mode: "cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
+    })
+      .then(resp => {
+        console.log("Ok:" + resp.status);
+      })
+      .catch(error => {
+        if (error.name === "AbortError") {
+          console.log("Fetch aborted");
+        } else {
+          console.error("Uh oh, an error!", error);
+        }
+      });
+  };
+
+  const handleSignalCancel = () => {
+    controller.abort();
+    init();
+  };
 
   // test if the input argument is a cancelable promise generator
-  if (cancelable(emptyPromise).cancel === undefined) {
-    throw new Error(
-      "promise wrapper argument must provide a cancel() function"
-    );
-  }
+  // if (cancelable(emptyPromise).cancel === undefined) {
+  //   throw new Error(
+  //     "promise wrapper argument must provide a cancel() function"
+  //   );
+  // }
 
-  const promises = useRef();
+  // const promises = useRef();
 
-  useEffect(() => {
-    promises.current = promises.current || [];
-    return function cancel() {
-      promises.current.forEach(p => p.cancel());
-      promises.current = [];
-    };
-  }, []);
+  // useEffect(() => {
+  //   promises.current = promises.current || [];
+  //   return function cancel() {
+  //     promises.current.forEach(p => p.cancel());
+  //     promises.current = [];
+  //   };
+  // }, []);
 
-  function cancellablePromise(p) {
-    const cPromise = cancelable(p);
-    promises.current.push(cPromise);
-    return cPromise.promise;
-  }
+  // function cancellablePromise(p) {
+  //   const cPromise = cancelable(p);
+  //   promises.current.push(cPromise);
+  //   return cPromise;
+  // }
 
-  return { cancellablePromise };
+  return { init, handleTriggerFetch, handleSignalCancel };
 }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import useCancellablePromise from "./contexts/useCancellablePromise";
 
 import "./styles.css";
 
 function App() {
-  const { cancellablePromise } = useCancellablePromise();
+  const { handleTriggerFetch, handleSignalCancel } = useCancellablePromise();
 
-  const createPromise = () => {};
+  // const createPromise = () => {
+  //   return fetch("https://tomini182.free.beeceptor.com/prueba", {
+  //     mode: "cors",
+  //     headers: {
+  //       "Access-Control-Allow-Origin": "*"
+  //     }
+  //   });
+  // };
 
-  const handleClick = () => {
-    console.log("abro");
-    const prueba = new Promise();
-    const test = cancellablePromise(prueba);
-    test.then(response => {
-      console.log("respuesta: " + response);
-    });
-    test.cancel();
-  };
+  // const handleClick = () => {
+  //   console.log("Pidiendo");
+  //   const prueba = createPromise();
+  //   handleTriggerFetch();
+  //   test.promise.then(response => {
+  //     console.log("respuesta: " + response.status);
+  //   });
+  // };
 
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <input type="button" value="enviar" onClick={handleClick} />
+      <input type="button" value="enviar" onClick={handleTriggerFetch} />
+      <input type="button" value="abortar" onClick={handleSignalCancel} />
     </div>
   );
 }
