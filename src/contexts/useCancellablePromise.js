27e509997bcfdef88f00ccb5b2fdf63ce7901091
Tomás Muñoz Testón <tomini18@hotmail.com>
2019-05-31 14:30:13 +0200

    Basic example

--- src/contexts/useCancellablePromise.js
+++ src/contexts/useCancellablePromise.js
@@ -1,31 +1,3 @@
-// export function makeCancelable(promise) {
-//   let isCanceled = false;
-
-//   const wrappedPromise = new Promise((resolve, reject) => {
-//     promise
-//       .then(val => (isCanceled ? console.log("cancelado") : resolve(val)))
-//       .catch(error =>
-//         isCanceled ? reject(new Error({ isCanceled })) : reject(error)
-//       );
-//   });
-
-//   return {
-//     promise: wrappedPromise,
-//     cancel() {
-//       isCanceled = true;
-//     }
-//   };
-// }
-
-// const createPromise = () => {
-//   return fetch("https://tomini182.free.beeceptor.com/prueba", {
-//     mode: "cors",
-//     headers: {
-//       "Access-Control-Allow-Origin": "*"
-//     }
-//   });
-// };
-
 export default function useCancellablePromise() {
   let controller;
 
@@ -60,29 +32,6 @@ export default function useCancellablePromise() {
     controller.abort();
     init();
   };
-
-  // test if the input argument is a cancelable promise generator
-  // if (cancelable(emptyPromise).cancel === undefined) {
-  //   throw new Error(
-  //     "promise wrapper argument must provide a cancel() function"
-  //   );
-  // }
-
-  // const promises = useRef();
-
-  // useEffect(() => {
-  //   promises.current = promises.current || [];
-  //   return function cancel() {
-  //     promises.current.forEach(p => p.cancel());
-  //     promises.current = [];
-  //   };
-  // }, []);
-
-  // function cancellablePromise(p) {
-  //   const cPromise = cancelable(p);
-  //   promises.current.push(cPromise);
-  //   return cPromise;
-  // }
 
   return { init, handleTriggerFetch, handleSignalCancel };
 }
--- src/index.js
+++ src/index.js
@@ -7,24 +7,6 @@ import "./styles.css";
 function App() {
   const { handleTriggerFetch, handleSignalCancel } = useCancellablePromise();
 
-  // const createPromise = () => {
-  //   return fetch("https://tomini182.free.beeceptor.com/prueba", {
-  //     mode: "cors",
-  //     headers: {
-  //       "Access-Control-Allow-Origin": "*"
-  //     }
-  //   });
-  // };
-
-  // const handleClick = () => {
-  //   console.log("Pidiendo");
-  //   const prueba = createPromise();
-  //   handleTriggerFetch();
-  //   test.promise.then(response => {
-  //     console.log("respuesta: " + response.status);
-  //   });
-  // };
-
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
